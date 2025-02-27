c0f3c2eb050c84ab30a9f8e54b5afcc980d1b209
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-02-27 15:14:30 -0300

    feat: create menu nav item

---
+++ src/components/ui/MainMenuNavItem.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { IMainMenuNavItem } from "@/types";
+
+defineProps<{
+	item: IMainMenuNavItem;
+}>();
+</script>
+
+<template>
+	<a
+		class="text-xl text-white underline-offset-6 hover:underline"
+		:href="item.to"
+	>
+		{{ item.label }}
+	</a>
+</template>
