9d6661a099014b5a1d35f7db3c127e2429e07ad7
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-02-27 15:14:49 -0300

    feat: create menu nav

---
+++ src/components/ui/MainMenuNav.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { IMainMenuNavItem } from "@/types";
+import MainMenuNavItem from "./MainMenuNavItem.vue";
+
+defineProps<{
+	items: IMainMenuNavItem[];
+}>();
+</script>
+
+<template>
+	<ul class="flex gap-8">
+		<li v-for="item in items" :key="item.to">
+			<MainMenuNavItem :item="item" />
+		</li>
+	</ul>
+</template>
