38359f8543b7558e9adcf5a56c2251ff770f4e3a
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-02-27 18:31:06 -0300

    feat: create best places component

---
+++ src/components/hero/BestPlaces.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+	year: string;
+	link?: string;
+}>();
+</script>
+
+<template>
+	<p class="max-sm:mb-4">
+		Melhores locais para visitar
+		<a
+			class="underline decoration-2 underline-offset-4 hover:no-underline"
+			:href="link || '#'"
+		>
+			{{ year }}
+		</a>
+	</p>
+</template>
