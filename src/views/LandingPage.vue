250cfa2206a3fd273c2191bc11c8b87f3df12e44
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-02-27 18:29:39 -0300

    feat: import hero section

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import HeroSection from "@/components/hero/HeroSection.vue";
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 </script>
 
 <template>
-	<DefaultLayout class="bg-moss-green-800"> 
-    
-  </DefaultLayout>
+	<DefaultLayout class="bg-moss-green-800">
+		<HeroSection />
+	</DefaultLayout>
 </template>
