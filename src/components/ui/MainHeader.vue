f18230eece2df3c68dae88cf076d92b7ab338caf
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-02-27 15:14:58 -0300

    feat: create header

---
+++ src/components/ui/MainHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { IMainMenuNavItem } from "@/types";
+import MainMenuNav from "./MainMenuNav.vue";
+import ForestLogo from "@/components/icons/ForestLogo.vue";
+
+const menuItems = [
+	{
+		to: "#",
+		label: "Acomodações",
+	},
+	{
+		to: "#",
+		label: "Eventos",
+	},
+	{
+		to: "#",
+		label: "Experiências",
+	},
+	{
+		to: "#",
+		label: "Contato",
+	},
+] satisfies IMainMenuNavItem[];
+</script>
+
+<template>
+	<header class="container flex items-center justify-between">
+		<ForestLogo />
+		<MainMenuNav :items="menuItems" />
+	</header>
+</template>
