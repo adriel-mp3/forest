7569502aaea174e29f3ebb63c778866c0dc03ec8
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-02-27 18:31:13 -0300

    feat: create recommend component

---
+++ src/components/hero/RecommendedBy.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  logoSrc: string;
+  logoAlt?: string;
+  publication: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <p class="mb-2 uppercase">Recomendado por</p>
+    <div class="flex items-center gap-4">
+      <img class="w-28" :src="logoSrc" :alt="logoAlt || 'Logo recomendação'" />
+      <span>|</span>
+      <span class="text-sm capitalize">{{ publication }}</span>
+    </div>
+  </div>
+</template>
